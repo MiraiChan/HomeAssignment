9bb2d49bf5b9edc6989cd3bc6352da30f480a477
Almira Khafizova <mimirka80@gmail.com>
2025-07-10 14:15:40 +0200

    Initial Commit

---
+++ HomeAssignment/HomeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAssignmentApp.swift
+//  HomeAssignment
+//
+//  Created by Almira Khafizova on 10.07.25.
+//
+
+import SwiftUI
+
+@main
+struct HomeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeAssignmentTests/HomeAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAssignmentTests.swift
+//  HomeAssignmentTests
+//
+//  Created by Almira Khafizova on 10.07.25.
+//
+
+import Testing
+@testable import HomeAssignment
+
+struct HomeAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
